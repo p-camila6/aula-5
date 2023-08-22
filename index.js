cc155fed81b0b6b9254a6bcd83adc8531d7c2070
p-camila6 <138184276+p-camila6@users.noreply.github.com>
2023-08-21 21:43:55 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,27 @@
+/* 
+*camila peres
+*Primeiro comentario
+*/ 
+console.log("Olá mundo!");
+//prompt("Digite seu nome");
+
+//variaveis 
+const variavelA = 100;
+const variavelB = variavelA + 50;
+console.log(variavelA);
+
+let numero = 50; 
+console.log(numero);
+numero = 80;
+console.log(numero);
+
+const nome = "Camila"
+const idade = 17
+
+console.log("Olá", "Meu nome é", nome, "e eu tenho", idade, "anos");
+
+const primeiro = "Camila"
+const sobrenome = "Peres" 
+const anos = "17"
+const sou = "estudante"
+console.log("Meu primeiro nome é", primeiro, "e meu sobrenome é", sobrenome, "tenho", anos, "anos", "atualmente sou", sou);
